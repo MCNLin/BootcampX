483d0dd088331c06c0dd2cfa12b99ef15977d541
Mei Nghiem-Lin <mcnghiem@gmail.com>
2021-11-23 23:46:58 +0000

    complete the rest of the questions in BootcampX Queries 4

--- 4_queries/9_cohort_avg_assistance_duration.sql
+++ 4_queries/9_cohort_avg_assistance_duration.sql
@@ -1,6 +1,10 @@
-SELECT SUM(completed_at - started_at) AS average_total_duration
-FROM assistance_requests
-JOIN students ON students.id = student_id
-JOIN cohorts ON cohorts.id = cohort_id
-GROUP BY cohorts.name
-ORDER BY average_total_duration;
+SELECT AVG(total_duration) AS average_total_duration
+FROM (
+  SELECT SUM(completed_at - started_at) AS total_duration
+  FROM assistance_requests
+  JOIN students ON students.id = student_id
+  JOIN cohorts ON cohorts.id = cohort_id
+  GROUP BY cohorts.name
+  ORDER BY total_duration
+) AS total_durations;
+
